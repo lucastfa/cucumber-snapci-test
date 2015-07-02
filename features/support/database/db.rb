9c2ec07e787edd6be9fa939f94676c839b7a9a91
Lucas Avila <lavila@thoughtworks.com>
2015-07-02 16:22:42 -0300

    Test 1

--- features/support/database/db.rb
+++ features/support/database/db.rb
@@ -3,12 +3,12 @@ require 'active_record'
 def estabilish_connection
 
 	# connect to the database
-	@db_connection = ActiveRecord::Base.establish_connection(
-										:adapter => 'postgresql',
-                                        :host => ENV['SNAP_DB_PG_HOST'],
-                                        :database => 'pgdatabase',
-                                        :username => ENV['SNAP_DB_PG_USER'],
-                                        :password => ENV['SNAP_DB_PG_PASSWORD'],
-                                        :port => ENV['SNAP_DB_PG_PORT']);
+	@db_connection = ActiveRecord::Base.establish_connection(ENV['SNAP_DB_PG_URL']);
+#										:adapter => 'postgresql',
+#                                        :host => ENV['SNAP_DB_PG_HOST'],
+#                                        :database => 'pgdatabase',
+#                                        :username => ENV['SNAP_DB_PG_USER'],
+#                                        :password => ENV['SNAP_DB_PG_PASSWORD'],
+#                                        :port => ENV['SNAP_DB_PG_PORT']);
 end
 
