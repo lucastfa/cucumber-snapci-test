c7da6cfd9884fe00690016397760fd467c983158
Lucas Avila <lavila@thoughtworks.com>
2015-07-02 15:59:25 -0300

    Test 1

--- features/support/database/db.rb
+++ features/support/database/db.rb
@@ -3,11 +3,11 @@ require 'active_record'
 def estabilish_connection
 
 	# connect to the database
-#	@db_connection = ActiveRecord::Base.establish_connection(
-#										:adapter => 'postgresql',
-#                                        :host => 'localhost');
-#                                        :database => 'lavila',
-#                                        :username => 'lavila',
-#                                        :password => 'lavila');
+	@db_connection = ActiveRecord::Base.establish_connection(
+										:adapter => 'postgresql',
+                                        :host => ENV['SNAP_DB_PG_HOST'],
+                                        :username => ENV['SNAP_DB_PG_USER'],
+                                        :password => ENV['SNAP_DB_PG_PASSWORD'],
+                                        :port => ENV['SNAP_DB_PG_PORT']);
 end
 
