d722ac4ddff730bef89cb5bc09f3f887ef50f487
Lucas Avila <lavila@thoughtworks.com>
2015-07-02 15:47:29 -0300

    Test

--- features/support/database/db.rb
+++ features/support/database/db.rb
@@ -5,9 +5,9 @@ def estabilish_connection
 	# connect to the database
 	@db_connection = ActiveRecord::Base.establish_connection(
 										:adapter => 'postgresql',
-                                        :host => 'localhost',
-                                        :database => 'lavila',
-                                        :username => 'lavila',
-                                        :password => 'lavila');
+                                        :host => 'localhost');
+#                                        :database => 'lavila',
+#                                        :username => 'lavila',
+#                                        :password => 'lavila');
 end
 
